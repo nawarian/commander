060b69d62f700b47c605ec5ad83775aceb17d6e7
nawarian <nickolas@phpsp.org.br>
2024-06-22 17:29:10 +0200

    Initial commit

---
+++ firefox-extension/dist.js
@@ -0,0 +1,30 @@
+browser.contextMenus.create(
+  {
+    id: "open-video",
+    title: "Open video",
+    contexts: ["link"],
+  },
+  () => void browser.runtime.lastError,
+);
+
+async function handleOpenMpvVideo(info, tab) {
+  const youtubeUrl = new URL(info.linkUrl)
+
+  const url = new URL(`http://localhost:7171/youtube?format=${youtubeUrl.searchParams.get('format')}&video=${youtubeUrl.searchParams.get('v')}`)
+  await fetch(url, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    }
+  });
+}
+
+browser.contextMenus.onClicked.addListener(async (info, tab) => {
+  switch (info.menuItemId) {
+    case "open-video":
+      await handleOpenMpvVideo(info, tab);
+      break;
+    default:
+      break;
+  }
+});
---
+++ server/src/main.rs
@@ -0,0 +1,62 @@
+use actix_web::{web, App, HttpServer, Responder, HttpRequest, HttpResponse};
+use actix_web::http::header;
+use serde::Deserialize;
+use std::process::{Command, Stdio};
+use tokio::task;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .wrap(
+                actix_cors::Cors::permissive()
+                    .allowed_origin_fn(|_origin, _req_head| true)
+                    // .supports_credentials()
+            )
+            .route("/whoami", web::get().to(whoami))
+            .route("/youtube", web::post().to(launch_youtube))
+    })
+    .bind("0.0.0.0:7171")?
+    .run()
+    .await
+}
+
+async fn whoami() -> impl Responder {
+    let output = Command::new("whoami")
+        .output()
+        .expect("failed to execute process");
+
+    let whoami = String::from_utf8_lossy(&output.stdout)
+        .trim()
+        .to_string();
+
+    HttpResponse::Ok().json(whoami)
+}
+
+#[derive(Deserialize)]
+struct LaunchYoutubeQueryParams {
+    video: Option<String>,
+    format: Option<String>,
+}
+
+async fn launch_youtube(req: HttpRequest) -> impl Responder {
+    let params: web::Query<LaunchYoutubeQueryParams> = web::Query::from_query(req.query_string()).unwrap();
+
+    let video = params.video.clone().unwrap().to_string();
+    let format = params.format.clone().unwrap().to_string();
+
+    println!("Launching youtube video {video} with format {format}");
+
+    let handle = task::spawn(async move {
+        let _ = Command::new("/usr/bin/mpv")
+            .arg(format!("https://www.youtube.com/watch?v={video} --ytdl-format={format}", video=video, format=format))
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .spawn();
+    });
+
+    match handle.await {
+        Ok(_) => HttpResponse::Ok(),
+        Err(_) => HttpResponse::InternalServerError(),
+    }
+}
